5ebfa9ebb8556f243fd286edb8854ae73496e81a
Felipe <felipefranca824@gmail.com>
2022-01-07 22:48:10 -0300

    Adicionado metodos toJson e fromJson aos models

--- lib/app/modules/home/data/models/category_model.dart
+++ lib/app/modules/home/data/models/category_model.dart
@@ -1,7 +1,10 @@
 import 'package:loja_clean/app/modules/home/domain/entities/category_entity.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'category_model.g.dart';
 
+@JsonSerializable()
 class CategoryModel extends CategoryEntity {
-  CategoryModel({
+  const CategoryModel({
     required String name,
   }) : super(name: name);
 }
--- lib/app/modules/home/data/models/product_model.dart
+++ lib/app/modules/home/data/models/product_model.dart
@@ -3,7 +3,6 @@ import 'package:loja_clean/app/modules/home/domain/entities/product_entity.dart'
 
 import 'category_model.dart';
 
-@JsonSerializable()
 class ProductModel extends ProductEntity {
   const ProductModel({
     required int id,
@@ -19,4 +18,25 @@ class ProductModel extends ProductEntity {
             category: category,
             price: price,
             image: image);
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+        id: json['id'],
+        title: json['title'],
+        description: json['description'],
+        category: json['category'],
+        price: json['price'],
+        image: json['image']);
+  }
+
+  Map<String, dynamic> toJson(ProductModel object) {
+    return <String, dynamic>{
+      'id': object.id,
+      'title': object.title,
+      'description': object.description,
+      'category': object.category,
+      'price': object.price,
+      'image': object.image,
+    };
+  }
 }
